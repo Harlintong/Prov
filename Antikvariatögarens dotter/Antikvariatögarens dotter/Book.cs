2dc4b385738032fede5950aacf270dccca9e6c95
Harlintong-NTIElev <55138427+Harlintong@users.noreply.github.com>
2019-10-22 12:55:53 +0200

    Add files via upload

---
+++ Antikvariatögarens dotter/Antikvariatögarens dotter/Book.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatögarens_dotter
+{
+    class Book
+    {
+        public int price;
+
+        int rarity;
+
+        int actualValue;
+
+        string name;
+
+        string category;
+        
+        bool curse;
+
+        List<string> booknames = new List<string>() { "The Truth", "Prophet", "100", "Universal", "Area 51" };
+        
+        Random generator = new Random();
+
+        public Book(string name)
+        {
+            name = booknames[generator.Next(4)];
+
+            rarity = generator.Next();
+
+            actualValue = generator.Next();
+
+            int c = generator.Next(1, 2);
+
+            if(c == 1)
+            {
+                curse = true;
+            }
+            else if(c == 2)
+            {
+                curse = false;
+            }
+
+        }
+        public void PrintInfo()
+        {
+
+            Console.WriteLine("Name: " + name);
+
+            Console.WriteLine("Rarity: " + rarity);
+
+            Console.WriteLine("Category: " + category);
+
+            Console.WriteLine("Price: " + price);
+
+        }
+        public int Evaluate()
+        {
+            int RPrice = actualValue * rarity;
+
+            return RPrice;
+
+        }
+        public string GetCategory()
+        {
+            return category;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public bool IsCursed()
+        {
+            return curse;
+        }
+
+    }
+}
---
+++ Antikvariatögarens dotter/Antikvariatögarens dotter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatögarens_dotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book();
+
+            book.PrintInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
